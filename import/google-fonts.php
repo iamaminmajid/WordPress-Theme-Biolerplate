1d099dbbc546f211543a49ebe7a4ff018cab5c44
iamaminmajid <me@aminmajid.com>
2016-07-14 03:50:42 +0500

    Update google-fonts.php

--- import/google-fonts.php
+++ import/google-fonts.php
@@ -1,7 +1,7 @@
 <?php
 function THEMENAME_fonts_url() {
     $font_url = '';
-    if ( 'off' !== _x( 'on', 'Google font: on or off', 'studio' ) ) {
+    if ( 'off' !== _x( 'on', 'Google font: on or off', 'THEMENAME' ) ) {
         $font_url = add_query_arg( 'family', urlencode( 'Roboto:300,400|Raleway:400,600,500,300|Roboto Slab|Cairo' ), "//fonts.googleapis.com/css" );
     }   
     return $font_url;
