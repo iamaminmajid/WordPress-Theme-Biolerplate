5a7ff0d6fffc20d2ac0bdab81bb198f6cbdbeece
Fahad Murtaza <fahdi@users.noreply.github.com>
2016-07-14 21:45:35 +0400

    Adding Bower to the theme development and distribution process (#1)
    * Adding gitignore, some sass + IDE rules, bower and future packaging rules. Jetbrains IDE rules are also added
    
    * Adding bower.json and initializing it with basic info about the project and dependencies as of now
    
    * Adding themify icons
    
    * Ignoring phpstorm IDE settings completely
    
    * Adding bower componenents' path instead of static one. Still need future improvement while creating automated build process
    
    * To do item added within code where refatcoring would be needed in future
    
    * Code formatting
    
    * Deleting the bootstrap file made redundant by bower
    
    * Removed redundant font files, updated enqueue script to support themify override which will be refatored later to support auotomated buulding
    
    * Code formatting
    
    * Removing bootstrap font files made redundant by bower
    
    * Cleaning up header
    
    * Cleaning up index file
    
    * Code cleanup and formatting in functions file
    
    * Some more cleanup in functions.php
    
    * Deleting redundant JS file from bootstrap
    
    * Updating readme to reflect new changes

--- functions.php
+++ functions.php
@@ -1,34 +1,33 @@
-<?php //Theme Finctions
+<?php
+//Theme Functions
 
 //Theme Setup --------------------------------------------------------------
-if ( ! function_exists( 'THEMENAME_setup' ) ) :
-function THEMENAME_setup() {
-	load_theme_textdomain( 'THEMENAME' );
-    
-	add_theme_support( 'automatic-feed-links' );
+if (!function_exists('THEMENAME_setup')) :
+    function THEMENAME_setup()
+    {
+        load_theme_textdomain('THEMENAME');
+        add_theme_support('automatic-feed-links');
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        set_post_thumbnail_size(250, 100, true);
+        add_theme_support('html5', array(
+            'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+        ));
 
-	add_theme_support( 'title-tag' );
+        register_nav_menus(array(
+            'primary' => __('Primary Menu', 'THEMENAME'),
+            'footer' => __('Footer Menu', 'THEMENAME')
+        ));
+    }
 
-	add_theme_support( 'post-thumbnails' );
-	set_post_thumbnail_size( 250, 100, true );
-    
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-	) );
-
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'THEMENAME' ),
-		'footer'  => __( 'Footer Menu', 'THEMENAME' )
-	) );
-}
-add_action( 'after_setup_theme', 'THEMENAME_setup' );
+    add_action('after_setup_theme', 'THEMENAME_setup');
 endif;
 
 //Google Fonts ---------------------------------------------------------------
-require_once (get_template_directory() . '/import/google-fonts.php');
+require_once(get_template_directory() . '/import/google-fonts.php');
 
 //JavaScript & Style Files ---------------------------------------------------
-require_once (get_template_directory() . '/import/scripts.php');
+require_once(get_template_directory() . '/import/scripts.php');
 
 //Bootstrap NavWalker --------------------------------------------------------
-require_once (get_template_directory() . '/wp_bootstrap_navwalker.php');
+require_once(get_template_directory() . '/wp_bootstrap_navwalker.php');
--- import/scripts.php
+++ import/scripts.php
@@ -1,9 +1,15 @@
 <?php
-function THEMENAME_adding_scripts() {
+function THEMENAME_adding_scripts()
+{
     wp_enqueue_script('jquery');
-	wp_enqueue_script( 'bootstrap', get_template_directory_uri(). '/js/bootstrap.min.js' );
-	wp_enqueue_style( 'bootstrap.css', get_template_directory_uri() . '/css/bootstrap.min.css');
-	wp_enqueue_style( 'themify-icons', get_template_directory_uri() . '/css/themify-icons.css' );
-	wp_enqueue_style( 'stylesheet', get_stylesheet_uri() );
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js');
+    wp_enqueue_style('bootstrap.css', get_template_directory_uri() . '/bower_components/bootstrap/dist/css/bootstrap.min.css');
+    wp_enqueue_style('themify-icons', get_template_directory_uri() . '/bower_components/themify-icons/css/themify-icons.css');
+    wp_enqueue_style('themify-icons-override', get_template_directory_uri() . '/css/themify-icons.css');
+    wp_enqueue_style('stylesheet', get_stylesheet_uri());
 }
-add_action( 'wp_enqueue_scripts', 'THEMENAME_adding_scripts' );
+
+add_action('wp_enqueue_scripts', 'THEMENAME_adding_scripts');
+
+//TODO: Future change for automated builds planned and this will change for that.
+//TODO: Support automated bower builds as in example at https://github.com/lykmapipo/themify-icons
