80701b049280c04630feff5319dec181d850988e
Fahad Murtaza <fahdim@gmail.com>
2016-07-14 05:06:30 +0400

    Adding bower componenents' path instead of static one. Still need future improvement while creating automated build process

--- import/scripts.php
+++ import/scripts.php
@@ -1,9 +1,9 @@
 <?php
 function THEMENAME_adding_scripts() {
     wp_enqueue_script('jquery');
-	wp_enqueue_script( 'bootstrap', get_template_directory_uri(). '/js/bootstrap.min.js' );
-	wp_enqueue_style( 'bootstrap.css', get_template_directory_uri() . '/css/bootstrap.min.css');
-	wp_enqueue_style( 'themify-icons', get_template_directory_uri() . '/css/themify-icons.css' );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri(). '/bower_components/bootstrap/dist/js/bootstrap.min.js' );
+	wp_enqueue_style( 'bootstrap.css', get_template_directory_uri() . '/bower_components/bootstrap/dist/css/bootstrap.min.css');
+	wp_enqueue_style( 'themify-icons', get_template_directory_uri() . '/bower_components/themify-icons/css/themify-icons.css' );
 	wp_enqueue_style( 'stylesheet', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'THEMENAME_adding_scripts' );
