59625f60c6ddf848e5e85d801d104a7cfe7137eb
Fahad Murtaza <fahdim@gmail.com>
2016-07-14 05:40:11 +0400

    Cleaning up header

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="#" type="image/x-icon" />
-        <?php wp_head(); ?>
-    </head>
-    <body <?php body_class(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="#" type="image/x-icon"/>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
         
